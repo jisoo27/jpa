6dec68a9b3417b18fc4ed3a5962be4a56bb2f869
jisu <jisuuoo96@gmail.com>
2022-03-30 16:31:00 +0900

    [ADD] : Member 객체 생성

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,27 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity // 로딩될 때 jpa를 사용하는 애구나 인식 -> 내가 관리해야겠다 고 인식
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
