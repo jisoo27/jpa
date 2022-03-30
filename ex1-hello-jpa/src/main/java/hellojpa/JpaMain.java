b6686c5f4394d08706868a9fd809fd7cc9b328f4
jisu <jisuuoo96@gmail.com>
2022-03-30 17:44:40 +0900

    [ADD] : 데이터베이스에서 조회 예제 - 1차 캐시에 없을 경우 DB조회 후 영속성 컨텍스트에 저장 - 1차 캐시에 있을 경우 거기서 반환됨 - 영속 엔티티의 동일성 보장(1차 캐시가 있기 때문에 가능)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,21 +17,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
-            //영속
-            System.out.println("=== BEFORE ===");
-
-            // 1차 캐시에 저장됨
-            em.persist(member);
-
-            System.out.println("=== AFTER ===");
-
-            //1차 캐시에서 조회
-            Member member1 = em.find(Member.class, 101L);
+            System.out.println("result = " + (findMember1 == findMember2));
 
             tx.commit();
         } catch (Exception e) {
