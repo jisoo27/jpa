b112fff40e796b0d33120aa658fe0646c68550f7
jisu <jisuuoo96@gmail.com>
2022-03-30 17:03:14 +0900

    [ADD] : JPQL 예제 추가 - JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공 - SQL과 문법 유사 - JPQL은 엔티티 객체를 대상으로 쿼리 - SQL은 데이터베이스 테이블을 대상으로 쿼리

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,8 +17,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HElloJPA");
+            //Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.Name = " + member.getName());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
