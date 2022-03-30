9a584245da73e16285c94c04787258d3324d8252
jisu <jisuuoo96@gmail.com>
2022-03-30 17:57:20 +0900

    [ADD] : 엔티티 등록 예제 - 트랜잭션을 지원하는 쓰기 지연 - 버퍼링을 모아 write 할 수 있다는 이점을 얻을 수 있다.

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,15 +14,21 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
-        tx.begin();
+        // 엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
+        tx.begin(); // [트랜잭션] 시작
 
         try {
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            System.out.println("result = " + (findMember1 == findMember2));
+            em.persist(member1);
+            em.persist(member2);
+            // 여기까지 INSERT SQL을 데이터베이스에 보내지 않는다.
+            // 이때 쿼리를 보내게 될 경우 최적화 할 수 있는 기회조차 없다.
+            System.out.println("===============");
 
-            tx.commit();
+            //커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
+            tx.commit(); // [트랜잭션] 커밋
         } catch (Exception e) {
             tx.rollback();
         } finally {
