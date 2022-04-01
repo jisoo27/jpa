5086cab2c80d58dcdc27ba1ea3ba7a27b137e77f
jisu <jisuuoo96@gmail.com>
2022-04-01 17:48:19 +0900

    [ADD] : 양방향 연관관계 주의 예제 - 순수 객체 상태를 고려해서 항상 양쪽에 값을 설정한다. - 연관관계 편의 메소드 생성(양쪽에 다 생성하면 안된다.) - 양방향 매핑시에 무한 루프를 조심해야한다.

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 
 public class JpaMain {
@@ -18,27 +17,18 @@ public class JpaMain {
         tx.begin();
 
         try {
-
             // 저장
             Team team = new Team();
             team.setName("TeamA");
-            em.persist(team); // 영속상태가 되면 무조건 pk 값이 세팅됨
+            em.persist(team);
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeam(team); // 단방향 연관관계 설정, 참조 저장
+            member.changeTeam(team); //**
             em.persist(member);
 
             em.flush();
-            em.clear(); // 이 2개의 과정을 거쳐줘야 DB에서 깔끔한 값을 가져온다.
-
-            // 조회
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for (Member m : members) {
-                System.out.println("m = " + m.getName());
-            }
+            em.clear();
 
             tx.commit();
         } catch (Exception e) {
