9ea8f187f5c47e0955fd4b7d6d022a43d3d086ad
jisu <jisuuoo96@gmail.com>
2022-04-05 17:37:39 +0900

    [ADD] : 일대다 양방향 예제

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -12,6 +12,9 @@ public class Member {
     @Column(name = "USERNAME")
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID",insertable = false, updatable = false)
+    private Team team;
 
     public Long getId() {
         return id;
