7db0604314db0ae10105bd19c019b9830c5abf56
jisu <jisuuoo96@gmail.com>
2023-01-22 23:36:28 +0900

    :sparkles: 회원 수정 실습 추가 - 다시 em.persistence 를 안해주어도 jpa가 commit 시점에 확인을 한다.   - 기존것과 달라지면 update 쿼리를 날려 변경해준다.

--- 기본편/src/main/java/hellojpa/JpaMain.java
+++ 기본편/src/main/java/hellojpa/JpaMain.java
@@ -15,9 +15,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            em.remove(findMember);
-
+            Member findMember = em.find(Member.class, 2L);
+            findMember.setName("HelloJPA");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
