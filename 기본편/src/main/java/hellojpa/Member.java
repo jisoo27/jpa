9a3ab0bf6247d5058afe4506376019302995458f
jisu <jisuuoo96@gmail.com>
2023-01-25 17:04:29 +0900

    :sparkles: 기본키 매핑 실습 추가 - SEQUENCE 전략 - 미리 값을 올려두는 방식

--- 기본편/src/main/java/hellojpa/Member.java
+++ 기본편/src/main/java/hellojpa/Member.java
@@ -3,17 +3,19 @@ package hellojpa;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR",
+sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+initialValue = 1, allocationSize = 1)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;// Pk가 뭔지 알려줘야한다.
 
     @Column(name = "name") // 데이터베이스 컬럼명을 설정해 줄 수 있다.
