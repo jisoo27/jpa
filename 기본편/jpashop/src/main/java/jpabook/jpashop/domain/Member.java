1ee52a4e081ea3ee89f3de4e396b34593f097743
jisu <jisuuoo96@gmail.com>
2023-01-25 18:01:55 +0900

    :sparkles: entity 생성 - 회원 - 주문 - 상품 - 주문 상품  - 현재의 코드는 객체지향스럽지 못하다.  - 객체는 참조로 쭉 이어져 있어야 한다.

---
+++ 기본편/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter // 예제이기 때문에
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+
+
+}
