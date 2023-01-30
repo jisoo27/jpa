8dea5c765e9e02547bcf3ce384ed53a0c5c95af5
jisu <jisuuoo96@gmail.com>
2023-01-31 04:22:20 +0900

    :sparkles: 고아 객체 제거 옵션 예제 실습 추가

--- 기본편/src/main/java/hellojpa/Parent.java
+++ 기본편/src/main/java/hellojpa/Parent.java
@@ -17,7 +17,7 @@ public class Parent {
 
     private String name;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Child> childList = new ArrayList<>(); // parent 가 Persist 될 때 이 컬렉션에 있는 모든 child 또한 자동으로 persist 되도록 해주는 옵션 CascadeType.ALL
 
     public void addChild(Child child) {
