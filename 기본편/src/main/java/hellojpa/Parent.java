fb824c3d3708210529c83436a332c66cbaa8742e
jisu <jisuuoo96@gmail.com>
2023-01-31 04:04:12 +0900

    :sparkles: 영속성 전이(CASCADE) 예제 실습 추가

---
+++ 기본편/src/main/java/hellojpa/Parent.java
@@ -0,0 +1,27 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Parent {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Child> childList = new ArrayList<>(); // parent 가 Persist 될 때 이 컬렉션에 있는 모든 child 또한 자동으로 persist 되도록 해주는 옵션 CascadeType.ALL
+
+    public void addChild(Child child) {
+        childList.add(child);
+        child.setParent(this);
+    }
+}
