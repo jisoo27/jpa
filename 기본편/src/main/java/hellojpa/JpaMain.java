a09131ff1076e91d4c6695a0a202539473f1aa2e
jisu <jisuuoo96@gmail.com>
2023-01-30 17:26:52 +0900

    :sparkles: 프록시 예제 실습 추가

--- 기본편/src/main/java/hellojpa/JpaMain.java
+++ 기본편/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,6 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,14 +14,18 @@ public class JpaMain {
         try {
 
             Member member = new Member();
-            member.setName("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
+            member.setName("hello");
+            
             em.persist(member);
-
+            
             em.flush();
             em.clear();
+            
+            
+//            Member findMember = em.find(Member.class, member.getId());
+            Member findMember = em.getReference(Member.class, member.getId()); // 가짜 프록시 객체 조회
+            System.out.println("findMember.getName() = " + findMember.getName()); // 내부적으로 영속성 컨텍스트에 요청을 하여 내부에 실제 target에 대한 값을 알게 되면서 getName의 값을 조회할 수 있다.
+
 
             tx.commit();
         } catch (Exception e) {
