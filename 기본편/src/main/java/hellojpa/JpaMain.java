d1a39b587afdd31824785a7f9ba2f266c828b872
jisu <jisuuoo96@gmail.com>
2023-02-02 14:40:10 +0900

    :sparkles: JPQL 실습 예제 - 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리 - SQL을 추상화해서 특정 데이터베이스 SQL에 의존 X - 실무에서는 QueryDSL 사용하는 것을 권장 - JPQL 을 잘 사용하면 QueryDSL은 어렵지 않다.

--- 기본편/src/main/java/hellojpa/JpaMain.java
+++ 기본편/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -12,35 +13,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("member1");
-            member.setHomeAddress(new Address("city1", "street", "10000"));
+            List<Member> resultList = em.createQuery("select m From Member m where m.name like '%kim%'", Member.class).getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("========START========");
-            Member findMember = em.find(Member.class, member.getId());
-
-//            //findMember.getHomeAddress().setCity("newCity"); X
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode())); // 수정 시 인스턴스 자체를 갈아끼워넣어야한다.
-//
-//            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000")); // equals 와 hashcode 가 제대로 구현되어 있으면 지워진다.
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            for (Member member : resultList) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         } catch (Exception e) {
