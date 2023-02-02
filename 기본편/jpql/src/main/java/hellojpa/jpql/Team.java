7ed0984fda738cfa6adbbfdb6d07150d9c295457
jisu <jisuuoo96@gmail.com>
2023-02-02 17:24:51 +0900

    :sparkles: 엔티티 생성 및 연관관계 매핑

---
+++ 기본편/jpql/src/main/java/hellojpa/jpql/Team.java
@@ -0,0 +1,26 @@
+package hellojpa.jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+}
