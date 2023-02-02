11b2ab3adcfcf53be64ebb157b91fea29318547b
jisu <jisuuoo96@gmail.com>
2023-02-02 15:21:25 +0900

    :sparkles: Native SQL 실습 예제

--- 기본편/src/main/java/hellojpa/JpaMain.java
+++ 기본편/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -16,16 +13,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //Criteria 사용 준비
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            //루트 클래스 (조회를 시작할 클래스)
-            Root<Member> m = query.from(Member.class);
-
-            //쿼리 생성
-            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
-            List<Member> resultList = em.createQuery(cq).getResultList();
+            List resultList = em.createNativeQuery("select MEMBER_ID, city, street from Member").getResultList();
 
             tx.commit();
         } catch (Exception e) {
