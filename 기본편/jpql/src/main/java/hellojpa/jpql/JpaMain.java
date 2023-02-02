16e5c7a83042e1d2394275353bab0afd73de6da2
jisu <jisuuoo96@gmail.com>
2023-02-02 17:50:14 +0900

    :sparkles: 임베디드 타입 프로젝션 예제

--- 기본편/jpql/src/main/java/hellojpa/jpql/JpaMain.java
+++ 기본편/jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -1,7 +1,6 @@
 package hellojpa.jpql;
 
 import javax.persistence.*;
-import java.util.List;
 
 public class JpaMain {
 
@@ -22,11 +21,8 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
+            em.createQuery("select o.address from Order o", Address.class)
                     .getResultList();
-
-            Member findMember = result.get(0);
-            findMember.setAge(20);
 
             tx.commit();
         } catch (Exception e) {
