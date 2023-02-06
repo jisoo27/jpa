62b29429e53c72b84a09d2939bec856cb13ad7b8
jisu <jisuuoo96@gmail.com>
2023-02-06 18:07:56 +0900

    :white_check_mark: 회원 repository 관련 기능 테스트 작성

---
+++ 실전! JPA 활용(1)/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception {
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
