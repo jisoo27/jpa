79646f62974270e4f521ccf955804bc85c93ed5a
jisu <jisuuoo96@gmail.com>
2023-02-07 19:41:26 +0900

    :sparkles: @ManyToOne 의 경우 EAGER 전략을 사용했을때 예시

--- 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/Order.java
+++ 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,9 +18,13 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member_id")
     private Member member;
+    // EAGER 일 경우 order를 조회할 때 member를 join해서 한번에 가져오는 경우가 있는데 => em.find()해서 한건 조회할 경우
+    // JPQL 로 select o From order o; 로 가져오게 되면 그대로 sql로 번역하게 된다. -> SQL select * from order
+    // 그럼 order가 100개가 있다면 100개를 다 가져오게 되고 member가 EAGER로 되어 있다면 100개가 100번 member를 가져오기 위해 단방쿼리가 100개 날아간다.
+    // 이것이 n+1(처음에 order를 가져오기 위한 쿼리) 문제이다. => 첫번째 날린 쿼리가 가져온 결과가 100개이면 n을 100으로 치환하면 된다.
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
