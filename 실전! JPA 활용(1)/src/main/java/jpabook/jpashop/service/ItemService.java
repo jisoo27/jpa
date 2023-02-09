956e273257edf5183c3b7fd4fb88d99b14769151
jisu <jisuuoo96@gmail.com>
2023-02-09 17:11:56 +0900

    :sparkles: 상품 service 기능 구현

---
+++ 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService { // 이렇게 repository 에 위임만 할 경우 꼭 service 를 꼭 만들어야 하는지 고민해봐야 한다. 컨트롤러에서 repository 에 바로 접근할 수 도 있다.
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+}
