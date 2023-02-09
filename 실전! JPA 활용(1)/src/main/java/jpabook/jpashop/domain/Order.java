4c86af2c607b22b5a1d1c1315fb0f317fb4835e2
jisu <jisuuoo96@gmail.com>
2023-02-09 22:35:03 +0900

    :sparkles: 주문, 주문상품 비즈니스 로직 추가

--- 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/Order.java
+++ 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -52,5 +53,39 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    // == 생성 메서드 == //
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직 ==//
+
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalArgumentException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) { // orderItem 이 여러개 일 경우 각각 cancel 해주어야한다.
+            orderItem.cancel();
+        }
+    }
+
+    // == 조회 로직 ==//
+
+    public int getTotalPrice() { // 전체 주문 가격 조회
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 
 }
--- 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,5 +26,25 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+    //==핵심 비즈니스 로직==
+
+    public void cancel() { // 취소 할 경우 재고 수량을 원복해주어야 한다.
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
+
+    // == 생성 메서드 ==
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
 
 }
