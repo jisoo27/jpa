fe5e43d5e3a4a2189deac8ae6b790746442cd0e4
jisu <jisuuoo96@gmail.com>
2023-02-09 17:03:49 +0900

    :sparkles: 상품 비즈니스 로직 추가

--- 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,21 @@ public abstract class Item { // 상속관계 전략을 부모테이블에 지정
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //===핵심 비즈니스 로직==//
+    // 도메인 주도 설계에서 엔티티 자체에서 해결할 수 있는 것들이있으면
+    // 엔티티 안에 비즈니스 로직을 넣는 것이 좋다.(객체지향적)
+    // setter 로 값 변경을 하는 것이 아닌 이러한 핵심 비지니스 메서드를 가지고 변경해야 한다.
+
+    public void addStock(int quantity) { // 재고 수량 증가
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) { // 재고 수량 감소
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
