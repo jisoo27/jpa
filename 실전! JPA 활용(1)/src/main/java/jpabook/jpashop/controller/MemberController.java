7bad1244ae56a63e0f6298cce01a9278037f0756
jisu <jisuuoo96@gmail.com>
2023-02-10 17:43:22 +0900

    :sparkles: 회원 목록 조회 api 구현

--- 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -4,6 +4,7 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,6 +43,14 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        model.addAttribute("members", memberService.findAll());
+        return "members/memberList";
+    }
+
+
 
 
 
