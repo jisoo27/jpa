27b1ccc1f14c52b25ef3ca322bf2da71da9f982d
jisu <jisuuoo96@gmail.com>
2023-02-10 18:02:10 +0900

    :sparkles: 상품 등록 api 구현

--- 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ 실전! JPA 활용(1)/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -2,9 +2,9 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.dto.MemberForm;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
