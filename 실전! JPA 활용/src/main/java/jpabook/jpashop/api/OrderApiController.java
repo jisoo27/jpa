48ee5535602a92d711f9f101e89aed57eada3998
jisu <jisuuoo96@gmail.com>
2023-02-22 19:05:28 +0900

    :sparkles: order 조회 컨트롤러 생성 [컬렉션 조회 최적화] - v2 : 엔티티를 DTO로 변환  - 지연 로딩으로 너무 많은 SQL 실행  - SQL 실행 수    - order 1번    -  member , address N번(order 조회 수 만큼)    - orderItem N번 (order 조회 수 만큼)    - item N번(orderItem 조회 수 만큼)

--- 실전! JPA 활용/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ 실전! JPA 활용/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,4 +33,51 @@ public class OrderApiController {
         }
         return all;
     }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        return orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            //orderItems = order.getOrderItems();
+            // orderItem 의 경우 null 값이 들어온다. 왜냐하면 getOrderItem() 은 Entity 그 자체이기 때문.
+            //order.getOrderItems().stream().forEach(o->o.getItem().getName()); // 이렇게 해줄 경우 값이 나오긴 하지만 entity 전체가 반환되는 것을 볼 수 있다.
+            // DTO 에서는 엔티티가 노출되면 안된다. 래핑하는 것도 안됨. 엔티티에 대한 의존을 완전히 끊어야 함
+            // orderItem 조차도 OrderDto 로 변환해주어야 한다.
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
 }
+
