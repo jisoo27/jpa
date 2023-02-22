035ace05a6b46ced8dde48fdb59d9ca947fe52cd
jisu <jisuuoo96@gmail.com>
2023-02-22 18:39:32 +0900

    :sparkles: order 조회 컨트롤러 생성 [컬렉션 조회 최적화] - v1 : 엔티티 직접 노출 - orderItem , item 관계를 직접 초기화하면 Hibernate5Module 설정에 의해 엔티티를 JSON으로 생성한다. - 양방향 연관관계면 무한 루프에 걸리지 않게 한곳에 @JsonIgnore 를 추가해야 한다. - 엔티티를 직접 노출하므로 좋은 방법은 아니다.

--- 실전! JPA 활용/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ 실전! JPA 활용/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -14,7 +14,7 @@ public class JpashopApplication {
 
 	@Bean
 	Hibernate5Module hibernate5Module() {
-		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		Hibernate5Module hibernate5Module = new Hibernate5Module(); // hibernate5Module 를 사용하여 Lazy 로딩을 호출해서 정상적으로 프록시가 초기화 된, 데이터가 로딩된 애들만 반 ㄱ
 		//hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true); // 사용하면 안되는 설정
 		return new Hibernate5Module();
 	}
