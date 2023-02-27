b99bab630785a37dc9775f20945eea46d16a086d
jisu <jisuuoo96@gmail.com>
2023-02-27 19:58:17 +0900

    :sparkles: member 엔티티, 레파지토리 구현

---
+++ 스프링 데이터 JPA/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,23 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
