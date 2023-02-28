378739cf1c399f8abc7ffcb6940984e093a4e679
jisu <jisuuoo96@gmail.com>
2023-02-28 18:12:59 +0900

    :sparkles: 순수 JPA 기반 리포지토리 구현

--- 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -19,6 +22,24 @@ public class MemberJpaRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member); // member 가 null일 수도 있고 아닐 수도 있음. Optional 로 한번 감싸서 제공
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
     }
 
 }
