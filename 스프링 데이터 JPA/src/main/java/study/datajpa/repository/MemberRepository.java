e80f95b7adea473e6c6ac266b29623b291cd33c8
jisu <jisuuoo96@gmail.com>
2023-03-03 15:03:49 +0900

    :sparkles: 여러가지 반환타입을 사용하여 구현

--- 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -27,5 +28,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") List<String> names);
+
+    //여러가지 반환타입 가능
+    List<Member> findListByUsername(String username); // 컬렉션
+    Member findMemberByUsername(String username); // 단건
+    Optional<Member> findOptionalByUsername(String username); // 단건 Optional
+
 
 }
