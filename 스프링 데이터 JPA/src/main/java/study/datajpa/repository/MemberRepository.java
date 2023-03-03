61e25f7e152146c964b45e28730c75368bf422f9
jisu <jisuuoo96@gmail.com>
2023-03-03 12:56:00 +0900

    :sparkles: @Query 로 값타입 또는 DTO 조회 구현

--- 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -17,4 +18,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m from Member m where m.username = :username and m.age = :age") // 많이 사용하는 기능
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
     // 엄청난 장점이 존재 : 문법오류가 있으면 컴파일 시점에 다 파악할 수 있다.
+
+    @Query("select m.username from Member m") // 엔티티가 아닌 값 타입 가져오기
+    List<String> findUsernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
 }
