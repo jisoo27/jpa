522f2520105b6fd1cde3605643904b187063c7ef
jisu <jisuuoo96@gmail.com>
2023-03-03 11:41:32 +0900

    :sparkles: @Query 로 레파지토 메소드에 쿼리 정의하여 구현

--- 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 스프링 데이터 JPA/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -13,4 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query(name = "Member.findByUsername") // namedQuery
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age") // 많이 사용하는 기능
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+    // 엄청난 장점이 존재 : 문법오류가 있으면 컴파일 시점에 다 파악할 수 있다.
 }
